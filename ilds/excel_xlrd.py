65529432e9adfe92a55a4b18cb58ab47b792a3f9
lds <85176878@qq.com>
2018-12-28 16:52:20 +0800

    20181228 在列表里面获取（歌曲名称）和（表演者）所在的行和列

--- ilds/excel_xlrd.py
+++ ilds/excel_xlrd.py
@@ -2,9 +2,9 @@
 #
 # ---------------------------------------
 #   程序：excel_xlrd.py
-#   版本：0.1
+#   版本：0.2
 #   作者：lds
-#   日期：2018-7-24
+#   日期：2018-12-28
 #   语言：Python 3.X
 #   说明：读取excel文件内容
 # ---------------------------------------
@@ -24,6 +24,9 @@ import xlrd
     XL_CELL_ERROR,
     XL_CELL_BLANK,
 ) = range(7)
+
+GEQU_TITLE_LIST = ['歌曲名', '歌曲', '歌名', '歌曲名称', '制品名称']
+GESHOU_TITLE_LIST = ['歌手名', '歌手', '演唱', '表演者', '艺人名称', '演唱者', '表演者名称', '艺人']
 
 
 class ReadXlsx(object):
@@ -67,10 +70,10 @@ class ReadXlsx(object):
 
         # 日期格式
         self.time_fmt = "%Y-%m-%d"
-            # self.time_fmt = "%Y-%m"
+        # self.time_fmt = "%Y-%m"
 
         self.excel = 'xls'
-        self.wb = xlrd.open_workbook(*args,**kwargs)
+        self.wb = xlrd.open_workbook(*args, **kwargs)
         self.sheet_names = self.wb.sheet_names()
         self.set_sheet(self.index)
         # print(self.excel)
@@ -78,7 +81,7 @@ class ReadXlsx(object):
     @property
     def title(self):
         """  标题 """
-        return self.sheet_names#[self.index]
+        return self.sheet_names  # [self.index]
 
     @property
     def max_row(self):
@@ -123,7 +126,7 @@ class ReadXlsx(object):
                 # print(cell.ctype)
                 if XL_CELL_DATE == cell.ctype:
                     # 把 excel 时间格式转化为 字符串
-                    cell_value =  xlrd.xldate.xldate_as_datetime(cell.value, 0).strftime(self.time_fmt)
+                    cell_value = xlrd.xldate.xldate_as_datetime(cell.value, 0).strftime(self.time_fmt)
                     # print('XL_CELL_DATE',cell_value)
                 elif XL_CELL_NUMBER == cell.ctype:
                     # 把 excel 数字转换为 字符串
@@ -145,7 +148,7 @@ class ReadXlsx(object):
         """ 下一个表 """
         return self.set_sheet(self.index + 1)
 
-    def set_sheet(self,index=None):
+    def set_sheet(self, index=None):
         """ 设置表 """
 
         if index is not None:
@@ -180,8 +183,8 @@ def gequ_re(gequ):
     最后修改时间：20181015
     """
     try:
-        gequ = gequ.replace('	', ' ').replace('(', '（').replace(')', '）').replace('\n', '')\
-            .replace('|', '/').replace(' ', ' ').replace(' （', '（')\
+        gequ = gequ.replace('	', ' ').replace('(', '（').replace(')', '）').replace('\n', '') \
+            .replace('|', '/').replace(' ', ' ').replace(' （', '（') \
             .replace('  ', ' ').replace('  ', ' ').strip()
         # .replace('/', ' ').replace('+', ' ')
     except Exception as e:
@@ -198,12 +201,55 @@ def geshou_re(geshou):
     """
     geshou = geshou.replace('+', ' ').replace('/', ' ').replace('	', ' ').replace('(', '（').replace(
         ')', '）').replace('&', ' ').replace('|', ' ').replace('•', '·') \
-        .replace('；', ' ').replace('\n', '').replace(',', ' ').replace(' ', ' ').replace(' （', '（')\
+        .replace('；', ' ').replace('\n', '').replace(',', ' ').replace(' ', ' ').replace(' （', '（') \
         .replace('  ', ' ').replace('  ', ' ').strip()
     if "（" in geshou:
         return geshou
     else:
         return geshou.replace('、', ' ').replace('  ', ' ')
+
+
+def get_gequ_geshou_line(gequ_list, one_gequ=False):
+    """在列表里面获取（歌曲名称）和（表演者）所在的行和列"""
+
+    id = 0  # 记录列数
+    line = 0
+    gequ_id = 0
+    geshou_id = 0
+
+    for row_value in gequ_list:
+        for i in row_value:
+            if i:  # 内容为空就直接跳过
+                i = str(i).strip()
+                # print (i)
+                # print (id)
+                for gequ_ in range(0, len(GEQU_TITLE_LIST)):  # 获取歌曲名
+                    # print (gequ[i])
+                    if i == GEQU_TITLE_LIST[gequ_]:
+                        # print (i, id, row_value)
+                        gequ_id = id
+                        # 是否只需要获取歌曲的行
+                        if one_gequ:
+                            return line, gequ_id
+                        else:
+                            id = 0  # 记录列数
+                            for i in row_value:
+                                if i:  # 内容为空就直接跳过
+                                    i = str(i).strip()
+                                    for geshou_ in range(0, len(GESHOU_TITLE_LIST)):  # 找到歌曲的时候才去获取歌手名
+                                        # print (geshou[i])
+                                        if i == GESHOU_TITLE_LIST[geshou_]:
+                                            # print (i, id)
+                                            geshou_id = id
+                                            return line, gequ_id, geshou_id
+                                id += 1
+            id += 1
+        id = 0
+        line += 1
+    if one_gequ:
+        return None, gequ_id
+    else:
+        return None, gequ_id, geshou_id
 
 
 def doc():
@@ -216,8 +262,10 @@ def doc():
     doc_text += '{fun.__name__}{fun.__doc__}\n'.format(fun=xlrd_string)
     doc_text += '{fun.__name__}{fun.__doc__}\n'.format(fun=gequ_re)
     doc_text += '{fun.__name__}{fun.__doc__}\n'.format(fun=geshou_re)
+    doc_text += '{fun.__name__}{fun.__doc__}\n'.format(fun=get_gequ_geshou_line)
 
     print(doc_text)
+
 
 if __name__ == "__main__":
     t1 = time()
@@ -239,9 +287,7 @@ if __name__ == "__main__":
 
     # print(xls.set_sheet(10)) # 这个要判断下是否成功
     print('next_sheet ---------------------------------')
-    print(xls.next_sheet(),xls.index)
+    print(xls.next_sheet(), xls.index)
     print(xls.title)
 
     print('用时 %.2f 秒' % (time() - t1))
-
-
